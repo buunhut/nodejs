7194647587bb0d65be227175b8c15a6c59dd7d47
Buu Nhut <nhut.nta@gmail.com>
2024-07-20 09:24:42 +0700

    fe

--- src/components/FormContact.js
+++ src/components/FormContact.js
@@ -12,6 +12,8 @@ const FormContact = () => {
     textMessage: "",
   });
 
+  const [process, setProcess] = useState(false);
+
   const handleChangeInput = (e) => {
     const { name, value } = e.target;
     setInfo((prevState) => ({ ...prevState, [name]: value }));
@@ -23,7 +25,7 @@ const FormContact = () => {
       setAlert((prevState) => ({ ...prevState, [name]: "" })); // Clear previous alert if input is valid
     }
   };
-  const handleSend = () => {
+  const handleSend = async () => {
     let isValid = true;
 
     // Check each field in info
@@ -37,52 +39,68 @@ const FormContact = () => {
     }
 
     if (isValid) {
-      console.log("send:", info);
+      setProcess(true);
+      setTimeout(() => {
+        setInfo({
+          yourName: "",
+          yourEmail: "",
+          textMessage: "",
+        });
+        setProcess(false);
+      }, 2000);
     }
   };
   return (
-    <div className="content">
-      <form action="">
-        <h2>Contact me</h2>
-        <div className="inputItem">
-          <input
-            type="text"
-            placeholder="Your name"
-            value={info.yourName}
-            name="yourName"
-            onChange={handleChangeInput}
-            onBlur={handleChangeInput}
-            style={{ borderColor: alert.yourName !== "" ? "red" : "" }}
-          />
-          <i className="fa-solid fa-user"></i>
+    <>
+      <div className="content">
+        <form action="">
+          <h2>Contact me</h2>
+          <div className="inputItem">
+            <input
+              type="text"
+              placeholder="Your name"
+              value={info.yourName}
+              name="yourName"
+              onChange={handleChangeInput}
+              onBlur={handleChangeInput}
+              style={{ borderColor: alert.yourName !== "" ? "red" : "" }}
+            />
+            <i className="fa-solid fa-user"></i>
+          </div>
+          <div className="inputItem">
+            <input
+              type="text"
+              placeholder="Your e-mail"
+              value={info.yourEmail}
+              name="yourEmail"
+              onChange={handleChangeInput}
+              onBlur={handleChangeInput}
+              style={{ borderColor: alert.yourEmail !== "" ? "red" : "" }}
+            />
+            <i className="fa-solid fa-envelope"></i>
+          </div>
+          <div className="inputItem">
+            <textarea
+              placeholder="Text message"
+              value={info.textMessage}
+              name="textMessage"
+              onChange={handleChangeInput}
+              onBlur={handleChangeInput}
+              style={{ borderColor: alert.textMessage !== "" ? "red" : "" }}
+            />
+          </div>
+          <button type="button" onClick={handleSend}>
+            <i className="fa-solid fa-paper-plane"></i> Send
+          </button>
+        </form>
+      </div>
+
+      {process && (
+        <div className="overlay">
+          <p>Sending...,</p>
         </div>
-        <div className="inputItem">
-          <input
-            type="text"
-            placeholder="Your e-mail"
-            value={info.yourEmail}
-            name="yourEmail"
-            onChange={handleChangeInput}
-            onBlur={handleChangeInput}
-            style={{ borderColor: alert.yourEmail !== "" ? "red" : "" }}
-          />
-          <i className="fa-solid fa-envelope"></i>
-        </div>
-        <div className="inputItem">
-          <textarea
-            placeholder="Text message"
-            value={info.textMessage}
-            name="textMessage"
-            onChange={handleChangeInput}
-            onBlur={handleChangeInput}
-            style={{ borderColor: alert.textMessage !== "" ? "red" : "" }}
-          />
-        </div>
-        <button type="button" onClick={handleSend}>
-          <i className="fa-solid fa-paper-plane"></i> Send
-        </button>
-      </form>
-    </div>
+      )}
+    </>
   );
 };
 
